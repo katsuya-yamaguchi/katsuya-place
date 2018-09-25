a33eed7b01ce9f6001557f055e10a25312113762
Katsuya003 <bb.win.3@gmail.com>
2018-09-26 05:30:22 +0900

    [update] mediaのマイグレーションファイルを更新

--- db/migrate/20180925200843_create_media.rb
+++ db/migrate/20180925200843_create_media.rb
@@ -1,7 +1,8 @@
 class CreateMedia < ActiveRecord::Migration[5.2]
   def change
     create_table :media do |t|
-
+      t.string :media_file_name, limit: 255
+      t.string :mdeia_url, limit: 255
       t.timestamps
     end
   end
