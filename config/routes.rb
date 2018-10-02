1e77595a99624124e296533a84df166938a49627
Katsuya003 <bb.win.3@gmail.com>
2018-10-03 05:29:05 +0900

    [update] articlesページへのルーティング制限を追加
    今の所、indexアクションしか使用しないためindexのみルーティングするように修正。

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'articles#index'
   devise_for :admin_users
   resources :categories
-  resources :articles
+  resources :articles, only: [:index]
   resources :media
   resources :admin_users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
