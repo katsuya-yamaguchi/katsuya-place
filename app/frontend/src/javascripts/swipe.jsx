4440eca412fe45fdea7056cc1a61b6f5d50ba386
Katsuya003 <bb.win.3@gmail.com>
2018-10-11 22:21:50 +0900

    [new] スライド機能を実装するための機能を追加
    https://github.com/nolimits4web/Swiper/blob/master/demos/030-pagination.html
    上記を参考にES6で実装。スライドがループするように設定。

---
+++ app/frontend/src/javascripts/swipe.jsx
@@ -0,0 +1,14 @@
+import Swiper from 'swiper';
+
+const mySwiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+})
