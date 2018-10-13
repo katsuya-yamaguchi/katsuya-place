82759435cb377eab427e72c2feb493fb0df03963
Katsuya003 <bb.win.3@gmail.com>
2018-10-13 16:21:44 +0900

    [update] ブログページへの動線を作成
    articles/index.html.erbからarticles/articles.html.erbへの動線を作成した。index.html.erbは一時的に開発用のhrefを使用している。

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :admin_users
   resources :categories
   resources :articles, only: [:index]
+  get '/articles/:id', to: 'articles#article'
   resources :media
   resources :admin_users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
