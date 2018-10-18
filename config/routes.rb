4bc93ffb626107ded4b4d32f2e2179ccec9968d7
Katsuya003 <bb.win.3@gmail.com>
2018-10-18 22:39:48 +0900

    [update] 管理画面で使用するルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,17 @@ Rails.application.routes.draw do
   resources :categories
   resources :articles, only: [:index]
   get '/articles/:id', to: 'articles#article'
-  resources :media
-  resources :admin_users
+
+  resources :admin_users do
+    resources :new_article, only: [:index, :create]
+    resources :new_fixed_article, only: [:index, :create]
+    resources :articles, only: [:index, :show, :create, :patch]
+    resources :fixed_article, only: [:index, :show, :create, :update]
+    resources :media, only: [:index, :show, :patch, :destroy]
+    namespace :media do
+      get '/upload', to: 'media#show'
+      post '/upload', to: 'media#create'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
