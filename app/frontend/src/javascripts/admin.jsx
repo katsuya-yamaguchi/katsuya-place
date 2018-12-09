b105e09b8d939597b5717cb718737d29e6433864
Katsuya003 <bb.win.3@gmail.com>
2018-12-10 06:29:34 +0900

    [new] 管理者ページのタブ切り替えJSの追加
    ページ遷移、タブの切り替え処理を追加。

---
+++ app/frontend/src/javascripts/admin.jsx
@@ -0,0 +1,32 @@
+/**
+ * @fileoverview /admin_users/:admin_user_id/*のHTMLページで使用する処理
+ */
+
+/**
+ * タブ要素(input)がクリックされたときに、ページ遷移するための処理
+ * @param {event} e クリックされた際のMouseEvent
+ * @returns {boolean} Window.location.hrefで遷移できたかどうか
+ */
+let transition = (e) => {
+  let dest = e.target.getAttribute('id').replace(/-/g, '_')
+  if(dest){
+    console.log('./' + dest)
+    window.location.href = './' + dest
+  }
+}
+
+/**
+ * タブ要素(input)のchecked状態を切り替えるための処理
+ * @param none
+ * @returns {boolean} 対象の要素にchecked属性を追加できたかどうか
+ */
+let changeTab = () => {
+  let path = location.pathname
+  let pathArr = path.split('/')
+  let currentPage = pathArr[pathArr.length - 1].replace(/_/g, '-')
+  let el = document.getElementById(currentPage)
+  el.setAttribute('checked', true)
+}
+
+changeTab()
+document.addEventListener('click', transition)
