8eed2a021b7d03d7e68c4eb1b85d602f5f60a250
Katsuya003 <bb.win.3@gmail.com>
2018-12-10 20:52:18 +0900

    [modify] 遷移するための処理を修正

--- app/frontend/src/javascripts/admin.jsx
+++ app/frontend/src/javascripts/admin.jsx
@@ -3,18 +3,6 @@
  */
 
 /**
- * タブ要素(input)がクリックされたときに、ページ遷移するための処理
- * @param {event} e クリックされた際のMouseEvent
- * @returns {boolean} Window.location.hrefで遷移できたかどうか
- */
-let transition = (e) => {
-  let dest = e.target.getAttribute('id').replace(/-/g, '_')
-  if(dest){
-    window.location.href = './' + dest
-  }
-}
-
-/**
  * タブ要素(input)のchecked状態を切り替えるための処理
  * @param none
  * @returns {boolean} 対象の要素にchecked属性を追加できたかどうか
@@ -28,4 +16,3 @@ let changeTab = () => {
 }
 
 changeTab()
-document.addEventListener('click', transition)
