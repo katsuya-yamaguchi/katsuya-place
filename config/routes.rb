12cd834897eda5d39eb92b6f339070e444d0b111
Katsuya003 <bb.win.3@gmail.com>
2018-12-13 21:52:34 +0900

    [modify] /meida/uploadへのルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,10 @@ Rails.application.routes.draw do
     resources :new_fixed_article, only: [:index, :create]
     resources :articles, only: [:index, :show, :create, :patch]
     resources :fixed_article, only: [:index, :show, :create, :update]
+
+    get '/media/upload', to: 'media#index_upload'
+    post '/media/upload', to: 'media#create_upload'
     resources :media, only: [:index, :show, :patch, :destroy]
-    namespace :media do
-      get '/upload', to: '#index_upload'
-      post '/upload', to: '#create_upload'
-    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
