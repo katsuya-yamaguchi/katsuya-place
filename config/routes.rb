b968fbb48b2114988f898a7b6a50be7b9364d1b5
Katsuya003 <bb.win.3@gmail.com>
2018-12-21 21:45:27 +0900

    [add] loginアクションへのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,7 @@ Rails.application.routes.draw do
   end
 
   get '/login', to: 'sessions#index'
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
