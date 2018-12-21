28f2c1a42a806640937b2cecc2a2712d395b2566
Katsuya003 <bb.win.3@gmail.com>
2018-12-21 21:52:41 +0900

    [add] create、destroyアクションへのルーティングテストを追加

--- spec/routing/sessions_spec.rb
+++ spec/routing/sessions_spec.rb
@@ -5,5 +5,11 @@ RSpec.describe MediaController, type: :routing do
     it "routes to #index" do
       expect(:get => "/login").to route_to("sessions#index")
     end
+    it "routes to #create" do
+      expect(:post => "/login").to route_to("sessions#create")
+    end
+    it "routes to #destroy" do
+      expect(:delete => "/login").to route_to("sessions#destroy")
+    end
   end
 end
