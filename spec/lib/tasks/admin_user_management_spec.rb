2167d54584d5908909afec8e2ed01ec0e8dea76d
Katsuya003 <bb.win.3@gmail.com>
2018-12-25 05:58:52 +0900

    [add] 管理者ユーザ作成用のタスクを追加

--- spec/lib/tasks/admin_user_management_spec.rb
+++ spec/lib/tasks/admin_user_management_spec.rb
@@ -1,26 +1,35 @@
 require 'rails_helper'
 require 'rake'
 
-describe 'task :add_admin_user' do
+describe ':admin_user_management' do
   before(:all) do
     @rake = Rake::Application.new
     Rake.application = @rake
     Rake.application.rake_require 'tasks/admin_user_management'
-    Rake::Task.define_task(:environent)
+    Rake::Task.define_task(:environment)
   end
-  context 'when create admin user,' do
-    let(:user_params){
-      {
+
+  describe 'task :add_user' do
+    it 'is a succeess thad the user was added.' do
+  
+      # 事前に環境変数ADMIN_PASSWORDを設定しておく。
+      expect{ @rake['admin_user_management:add_user'].invoke }.to change{ AdminUser.count }.by(1)
+    end
+  end
+  
+  describe 'task :delete_user' do
+    it 'is a succeess that the user was deleted.' do
+      user_params = {
         id: 1,
         created_at: Time.new,
         updated_at: Time.new,
         email: 'admin@admin.com',
         password_digest: '123456789'
       }
-    }
-
-    it 'succeeds.' do
-      expect{ @rake['admin_user_management:add_user'].invoke }.to change{ AdminUser.count }.by(1)
+      user = AdminUser.new(user_params);
+      user.save!
+  
+      expect{ @rake['admin_user_management:delete_user'].invoke }.to change{ AdminUser.count }.from(1).to(0)
     end
   end
 end
