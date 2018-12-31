51bf0f906a8836d0566ce9182defb7441c8dd4c6
Katsuya003 <bb.win.3@gmail.com>
2018-12-31 15:36:02 +0900

    [update] email,password_digestに対して存在チェックのバリデーションを追加

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,4 +1,6 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :email, presence: true
+  validates :password_digest, presence: true
 end
--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe AdminUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    let(:admin_user){
+      AdminUser.new(email: 'sample@sample.com', password_digest: '123456789')
+    }
+
+    it 'should be valid.' do
+      expect(admin_user.valid?).to equal(true)
+    end
+
+    context 'when the name does not exist,' do
+      it 'validation fails.' do
+        admin_user.email = ''
+        expect(admin_user.valid?).to equal(false)
+      end
+    end
+
+    context 'when the password_digest does not exist,' do
+      it 'validation fails.' do
+        admin_user.password_digest = ''
+        expect(admin_user.valid?).to equal(false)
+      end
+    end
+  end
 end
