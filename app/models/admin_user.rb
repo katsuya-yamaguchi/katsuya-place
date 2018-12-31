0e98cf1a5c0527ba1cd5b042f27c71f26107af3c
Katsuya003 <bb.win.3@gmail.com>
2018-12-31 17:49:34 +0900

    [update] Add validation.

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,7 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :email, presence: true
-  validates :password_digest, presence: true
+  VALD_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALD_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password_digest, presence: true, length: { minimum: 8 }
 end
--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -10,17 +10,39 @@ RSpec.describe AdminUser, type: :model do
       expect(admin_user.valid?).to equal(true)
     end
 
-    context 'when the name does not exist,' do
+    context 'when the name does exist,' do
+      it 'validation success.' do
+        admin_user.email = 'sample@sample.com'
+        expect(admin_user.valid?).to equal(true)
+      end
+    end
+
+    context 'when the password_digest does exist,' do
+      it 'validation success.' do
+        admin_user.password_digest = '12345678'
+        expect(admin_user.valid?).to equal(true)
+      end
+    end
+
+    context 'when the length of password_digest is short,' do
       it 'validation fails.' do
-        admin_user.email = ''
+        admin_user.password_digest = '1234567'
         expect(admin_user.valid?).to equal(false)
       end
     end
 
-    context 'when the password_digest does not exist,' do
+    context 'when the email format is invalid,' do
       it 'validation fails.' do
-        admin_user.password_digest = ''
+        admin_user.email = 'admin'
         expect(admin_user.valid?).to equal(false)
+      end
+    end
+
+    context 'when the email is duplicated,' do
+      it 'validation fails.' do
+        admin_user.save!
+        dup_admin_user = AdminUser.new(email: 'sample@sample.com', password_digest: '123456789')
+        expect(dup_admin_user.valid?).to equal(false)
       end
     end
   end
