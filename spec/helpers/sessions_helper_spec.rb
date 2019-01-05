46b6ed80e31f51fd0db84d9d39353a811e1e9003
Katsuya003 <bb.win.3@gmail.com>
2019-01-05 23:41:06 +0900

    [update] helperのテストをmodelに移行
    記載場所を間違えていたため。

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -1,39 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe SessionsHelper, type: :helper do
-  let(:user){
-    user_params = {
-      id: 1,
-      created_at: Time.new,
-      updated_at: Time.new,
-      email: 'admin@admin.com',
-      password: '123456789',
-      password_confirmation: '123456789',
-      remember_digest: 'aaaa'
-    }
-    AdminUser.new(user_params);
-  }
-
-  describe '#AdminUser.digest' do
-    it 'successful generation of token.' do
-      sample_text = '123456789'
-      expect(AdminUser.digest(sample_text)).not_to eq nil
-    end
-  end
-
-  describe '#remember' do
-    it 'update of token succeeds.' do
-      user.remember
-      expect(user.remember_digest).not_to eq 'aaaa'
-    end
-  end
-
-  describe '#authenticated?' do
-    context 'when the token matches the digest,' do
-      it 'return true.' do
-        user.remember
-        expect(user.authenticated?(user.remember_token)).to eq true
-      end
-    end
-  end
 end
--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -1,6 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe AdminUser, type: :model do
+  let(:user){
+    user_params = {
+      id: 1,
+      created_at: Time.new,
+      updated_at: Time.new,
+      email: 'admin@admin.com',
+      password: '123456789',
+      password_confirmation: '123456789',
+      remember_digest: 'aaaa'
+    }
+    AdminUser.new(user_params);
+  }
+
   describe 'validation' do
     let(:admin_user){
       AdminUser.new(email: 'sample@sample.com', password: '123456789', password_confirmation: '123456789')
@@ -36,6 +49,29 @@ RSpec.describe AdminUser, type: :model do
         admin_user.save!
         dup_admin_user = AdminUser.new(email: 'sample@sample.com', password_digest: '123456789')
         expect(dup_admin_user.valid?).to equal(false)
+      end
+    end
+  end
+
+  describe '#AdminUser.digest' do
+    it 'successful generation of token.' do
+      sample_text = '123456789'
+      expect(AdminUser.digest(sample_text)).not_to eq nil
+    end
+  end
+
+  describe '#remember' do
+    it 'update of token succeeds.' do
+      user.remember
+      expect(user.remember_digest).not_to eq 'aaaa'
+    end
+  end
+
+  describe '#authenticated?' do
+    context 'when the token matches the digest,' do
+      it 'return true.' do
+        user.remember
+        expect(user.authenticated?(user.remember_token)).to eq true
       end
     end
   end
