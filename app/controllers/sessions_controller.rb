732daa5652d0f5e84bbe6f893b11a0a4667caf31
Katsuya003 <bb.win.3@gmail.com>
2019-01-07 05:49:05 +0900

    [update] セッションID、cookieの値の名称を修正 fixed #9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,21 @@
 class SessionsController < ApplicationController
   def index
-    if session[:admin_user_id]
-      session.delete(:admin_user_id)
+    if session[:user_id]
+      session.delete(:user_id)
     end
   end
 
   def create
     user = AdminUser.find_by(email: params[:sessions][:email].downcase)
 
-    if session[:admin_user_id]
-      session.delete(:admin_user_id)
+    if session[:ser_id]
+      session.delete(:user_id)
     end
 
     if user && user.authenticate(params[:sessions][:password])
-      session[:admin_user_id] = user.id
+      session[:user_id] = user.id
       params[:sessions][:remember_me] == 1 ? remember(user) : forget(user)
-      redirect_to admin_user_new_article_index_path(admin_user_id: session[:admin_user_id])
+      redirect_to admin_user_new_article_index_path(admin_user_id: session[:user_id])
     else
       flash.now[:danger] = 'email or password is invalid.'
       render :index
