3ff17a8b8ea1ea07c04bc82775a3838fc2793fa0
Katsuya003 <bb.win.3@gmail.com>
2019-01-15 10:58:28 +0900

    [update] 記事作成処理をhelperにて共通化　#12

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,22 @@
 module ArticlesHelper
+  def article_params
+    params.require(:articles).permit(
+      :content_title,
+      :content_url,
+      :meta_description,
+      :content_text,
+      :open_status
+    )
+  end
+
+  def category_params
+    params.require(:articles).permit(:category_name)
+  end
+
+  def create_article
+    category = Category.new(category_params)
+    category.save!
+    article = category.article.build(article_params)
+    article.save!
+  end
 end
