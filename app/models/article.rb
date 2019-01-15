85760123e8f224e518f6197f7bedcf460de8898c
Katsuya003 <bb.win.3@gmail.com>
2019-01-15 10:34:56 +0900

    [update] articleモデルにバリデーションを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ApplicationRecord
-  belongs_to :category, class_name: 'Category'
+  belongs_to :category, class_name: 'Category', foreign_key: 'categories_id'
+  validates :content_title, presence: true
+  validates :content_url, presence: true
+  validates :content_text, presence: true
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#validates' do
+    let(:article_params){
+      {
+        content_title: 'sample',
+        content_url: 'https://sample.com',
+        meta_description: 'sample',
+        content_text: 'sample',
+      }
+    }
+
+    let(:category){
+      Category.create(category_name: 'test')
+    }
+
+    context ':content_titleが存在していない時、' do
+      it 'バリデーションが失敗すること' do
+        article_params[:content_title] = ''
+        article = category.article.new(article_params)
+        expect(article.valid?).to eq false
+      end
+    end
+
+    context ':content_urlが存在していない時、' do
+      it 'バリデーションが失敗すること' do
+        article_params[:content_url] = ''
+        article = category.article.new(article_params)
+        expect(article.valid?).to eq false
+      end
+    end
+
+    context ':content_textが存在していない時、' do
+      it 'バリデーションが失敗すること' do
+        article_params[:content_text] = ''
+        article = category.article.new(article_params)
+        expect(article.valid?).to eq false
+      end
+    end
+  end
 end
