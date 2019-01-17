068f61bbf7528a6324fd7c2af2fe3c57fc86156f
Katsuya003 <bb.win.3@gmail.com>
2019-01-17 21:02:55 +0900

    [update] トップページの修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,63 +1,29 @@
 class ArticlesController < ApplicationController
-  before_action :check_login_status, only: [:index, :create]
+  #before_action :check_login_status, only: [:index, :create]
 
   # GET /articles
-  # GET /articles.json
   def index
-    @articles = Article.all
-  end
-
-  # GET /articles/:id
-  def article
-    render 'articles'
+    @famous_articles = Article.where(famous_status: 1)
+    @articles = Article.order(:updated_at)
   end
 
   # GET /articles/1
-  # GET /articles/1.json
   def show
-  end
-
-  # GET /articles/new
-  def new
-    @article = Article.new
-  end
-
-  # GET /articles/1/edit
-  def edit
-  end
-
-  # POST /articles
-  # POST /articles.json
-  def create
-    @article = Article.new(article_params)
-
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /articles/1
-  # PATCH/PUT /articles/1.json
-  def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    article_url = '/'
+    article_url << get_article_url
+    @article = ''
+    Article.where(content_url: article_url).each do |article|
+      @article = article
     end
   end
 
   private
     def check_login_status
       logged_in?
+    end
+
+    # 「/articles/test」の最後のurlを取得する処理
+    def get_article_url
+      request.fullpath.split('/').last
     end
 end
