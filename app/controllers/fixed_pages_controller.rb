e95a2269054cbafb483fe384f6de813a69199037
Katsuya003 <bb.win.3@gmail.com>
2019-02-03 17:28:01 +0900

    [update] 各種修正

--- app/controllers/fixed_pages_controller.rb
+++ app/controllers/fixed_pages_controller.rb
@@ -1,2 +1,11 @@
 class FixedPagesController < ApplicationController
+
+  # GET /categories
+  def category
+    @categories = Category.order(:updated_at)
+  end
+
+  # GET /profile
+  def profile
+  end
 end
