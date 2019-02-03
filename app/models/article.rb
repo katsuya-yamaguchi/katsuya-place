57476e140497727641c8eab8b141f1ead59f0c04
Katsuya003 <bb.win.3@gmail.com>
2019-02-03 18:33:29 +0900

    [update] #24 ArticleテーブルとMediaテーブルにリレーションを追加
    articleからmediaが呼び出せるようにリレーションを追加した。

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :category, class_name: 'Category', foreign_key: 'categories_id'
+  belongs_to :medium, class_name: 'Medium', foreign_key: 'media_id'
   validates :content_title, presence: true
   validates :content_url, presence: true
   validates :content_text, presence: true
