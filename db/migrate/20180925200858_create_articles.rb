67151a522f51fa2b2fddf8731d21b2407aba3a70
Katsuya003 <bb.win.3@gmail.com>
2019-02-27 20:51:53 +0900

    [update] テーブル作成順を修正

--- db/migrate/20180925200858_create_articles.rb
+++ db/migrate/20180925200858_create_articles.rb
@@ -1,9 +1,14 @@
-class CreateCategories < ActiveRecord::Migration[5.2]
+class CreateArticles < ActiveRecord::Migration[5.2]
   def change
-    create_table :categories do |t|
-      t.string :category_name, limit: 255, null: false
+    create_table :articles do |t|
+      t.text :content_text
+      t.text :meta_description
+      t.string :content_url, limit: 255, null: false
+      t.string :content_title, limit: 255
+      #t.references :categories, foreign_key: true
+      t.references :media, foreign_key: true
       t.timestamps
     end
-    add_index :categories, [:category_name], unique: true
+    add_index :articles, [:content_url], unique: true
   end
 end
--- db/migrate/20190117111024_add_famous_status_to_article.rb
+++ db/migrate/20190117111024_add_famous_status_to_article.rb
@@ -1,5 +1,6 @@
 class AddFamousStatusToArticle < ActiveRecord::Migration[5.2]
   def change
     add_column :articles, :famous_status, :integer, default: 0
+    add_reference :articles, :categories, foreign_key: true 
   end
 end
