9634c501563e0c4273f0078915a9e93177d90f93
Katsuya003 <bb.win.3@gmail.com>
2019-03-18 15:17:27 +0900

    [update] #37 テーブルをmysqlで再作成

--- db/schema.rb
+++ db/schema.rb
@@ -12,17 +12,17 @@
 
 ActiveRecord::Schema.define(version: 2019_01_17_111024) do
 
-  create_table "active_storage_attachments", force: :cascade do |t|
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", force: :cascade do |t|
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2019_01_17_111024) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "admin_users", force: :cascade do |t|
+  create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
@@ -41,35 +41,37 @@ ActiveRecord::Schema.define(version: 2019_01_17_111024) do
     t.string "remember_digest"
   end
 
-  create_table "articles", force: :cascade do |t|
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content_text"
     t.text "meta_description"
-    t.string "content_url", limit: 255, null: false
-    t.string "content_title", limit: 255
-    t.integer "categories_id"
-    t.integer "media_id"
+    t.string "content_url", null: false
+    t.string "content_title"
+    t.bigint "media_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "open_status", default: 0
     t.integer "fixed_status", default: 0
     t.integer "famous_status", default: 0
+    t.bigint "categories_id"
     t.index ["categories_id"], name: "index_articles_on_categories_id"
     t.index ["content_url"], name: "index_articles_on_content_url", unique: true
     t.index ["media_id"], name: "index_articles_on_media_id"
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "category_name", limit: 255, null: false
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "category_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_name"], name: "index_categories_on_category_name", unique: true
   end
 
-  create_table "media", force: :cascade do |t|
-    t.string "media_file_name", limit: 255
-    t.string "mdeia_url", limit: 255
+  create_table "media", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "media_file_name"
+    t.string "mdeia_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "articles", "categories", column: "categories_id"
+  add_foreign_key "articles", "media", column: "media_id"
 end
