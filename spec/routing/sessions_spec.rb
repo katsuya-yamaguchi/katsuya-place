997ee366331c63c8de3ea34247bd0be635b5e472
Katsuya003 <bb.win.3@gmail.com>
2019-04-27 22:08:32 +0900

    クォーテーションの誤記を修正

--- spec/routing/sessions_spec.rb
+++ spec/routing/sessions_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe MediaController, type: :routing do
   describe 'routing' do
-    it 'routes to #index" do
+    it 'routes to #index' do
       expect(:get => '/login').to route_to('sessions#index')
     end
     it 'routes to #create' do
