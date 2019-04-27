06026d9604fb428fb6bb872a34ccfdd62c315dbd
Katsuya003 <bb.win.3@gmail.com>
2019-04-27 21:53:20 +0900

    #50 定数にfreezeメソッドを追加
    freezeメソッドはオブジェクトを凍結させる役割。凍結させたオブジェクトを変更するとエラーが発生する。

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,6 @@
 class AdminUser < ApplicationRecord
   attr_accessor :remember_token
-  VALD_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALD_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, format: { with: VALD_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password_digest, length: { minimum: 8 }
   has_secure_password
