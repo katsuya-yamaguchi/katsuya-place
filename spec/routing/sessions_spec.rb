8a147de8424a74575c650edf2418c800f71539c9
Katsuya003 <bb.win.3@gmail.com>
2019-05-08 05:44:38 +0900

    hashの記法を修正

--- spec/routing/sessions_spec.rb
+++ spec/routing/sessions_spec.rb
@@ -1,15 +1,15 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe MediaController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(:get => '/login').to route_to('sessions#index')
+      expect(get: '/login').to route_to('sessions#index')
     end
     it 'routes to #create' do
-      expect(:post => '/login').to route_to('sessions#create')
+      expect(post: '/login').to route_to('sessions#create')
     end
     it 'routes to #destroy' do
-      expect(:delete => '/login').to route_to('sessions#destroy')
+      expect(delete: '/login').to route_to('sessions#destroy')
     end
   end
 end
